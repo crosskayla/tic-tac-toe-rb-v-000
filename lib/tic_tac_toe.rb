af6f5ddf627a326f536728a66cec2fdf9a6b24e7
Kayla Cross <kac288@georgetown.edu>
2018-02-05 01:06:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,6 +58,14 @@ def turn_count(board)
   return taken_positions.length
 end
 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) %2 == 1
+    return "O"
+  end
+end
+
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 
 puts turn_count(board)
